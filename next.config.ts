fd14be6d8996e5f30aadc143f46b5b1897b3c7d5
phoenix7100 <garcjorge1117@gmail.com>
2026-02-28 16:05:57 -0500

    took off eslint ignoreDuringBuilds: true,

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev','http://localhost:3000', '192.168.1.2'],
-  typescript: { ignoreBuildErrors: true },
 };
 
 module.exports = nextConfig;
