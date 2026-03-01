eea546de121bb724f40ddec7ff0c9e2c4fa6be35
phoenix7100 <garcjorge1117@gmail.com>
2026-02-28 21:40:57 -0500

    lib

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,5 +1,39 @@
 import { MongoClient } from "mongodb";
 
+const uri = process.env.MONGO_URI!;
+if (!uri) {
+  throw new Error("Missing MONGO_URI");
+}
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+declare global {
+  // eslint-disable-next-line no-var
+  var _mongoClientPromise: Promise<MongoClient> | undefined;
+}
+
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri);
+  global._mongoClientPromise = client.connect();
+}
+
+clientPromise = global._mongoClientPromise;
+
+export default clientPromise;
+
+
+
+
+
+
+
+
+
+
+
+/*import { MongoClient } from "mongodb";
+
 if (!process.env.MONGO_URI) {
   throw new Error("Please add MONGO_URI to your environment variables");
 }
@@ -22,4 +56,4 @@ if (process.env.NODE_ENV === "development") {
   clientPromise = client.connect();
 }
 
-export default clientPromise;
+export default clientPromise; */
